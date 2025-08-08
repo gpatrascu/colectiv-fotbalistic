85fcbf2a121175de42fea0f5f5a96393c7d22e7b
George Patrascu <george.patrascu@yahoo.com>
2025-08-08 19:58:29 +0300

    add policy page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { PlayersPageComponent } from './players-page.component';
+import { PrivacyPolicyComponent } from './privacy-policy.component';
 
 export const routes: Routes = [
   { path: 'players', component: PlayersPageComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
+  { path: '', redirectTo: '/players', pathMatch: 'full' },
 ];
